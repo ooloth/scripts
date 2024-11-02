97690a1cb0a85a05cce2f53f308739a7c7d4a949
Michael Uloth <hello@michaeluloth.com>
2024-11-02 17:21:03 -0400

    sendgrid: get secrets from 1Password at runtime

--- notifications/sendgrid.py
+++ notifications/sendgrid.py
@@ -1,15 +1,10 @@
-# TODO: singleton client
-
-import os
-
 from sendgrid.helpers.mail import Mail  # type: ignore
 from sendgrid.sendgrid import SendGridAPIClient  # type: ignore
 
+from op.items import get_secret
 from utils.logs import log
 
-# Docs:
-# - https://github.com/sendgrid/sendgrid-python/blob/main/use_cases/README.md
-# - https://github.com/sendgrid/sendgrid-python/blob/main/use_cases/send_a_single_email_to_a_single_recipient.md
+OP_ITEM = "SendGrid"
 
 _client = None
 
@@ -20,31 +15,26 @@ def get_client() -> SendGridAPIClient:
     if _client:
         return _client
 
-    # TODO: locally and in GitHub Actions, should these pull from the op cli instead of the environment for safety?
-    _client = SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
+    _client = SendGridAPIClient(api_key=get_secret(OP_ITEM, "api key"))
 
     return _client
 
 
 def send_email(subject: str, html: str) -> None:
-    log("subject:", subject)
-    log("html:", html)
-
-    html_heading = "<h1>Script notification:</h1>"
-    html = f"{html_heading}{html}"
-
+    """
+    Send an email with a standard heading and the given subject and HTML content.
+    See: https://github.com/sendgrid/sendgrid-python/blob/main/use_cases/send_a_single_email_to_a_single_recipient.md
+    """
     message = Mail(
-        from_email=os.environ.get("SENDGRID_FROM_EMAIL"),
-        to_emails=os.environ.get("SENDGRID_TO_EMAIL"),
+        from_email=get_secret(OP_ITEM, "from email"),
+        to_emails=get_secret(OP_ITEM, "to email"),
         subject=subject,
         html_content=html,
     )
 
     try:
         client = get_client()
-        response = client.send(message)
-        log(response.status_code)
-        log(response.body)
-        log(response.headers)
+        client.send(message)
+        log("✅ Email sent successfully.")
     except Exception as e:
-        log("There was a problem sending that email:", e)
+        log("🚨 There was a problem sending that email:", e)
