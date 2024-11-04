3ea017a21b80d30cebd923fe16886c106920b339
Michael Uloth <hello@michaeluloth.com>
2024-11-03 19:31:40 -0500

    logs: simplify to basically just use rich.print directly for now

--- utils/logs.py
+++ utils/logs.py
@@ -3,10 +3,6 @@ import sys
 import rich
 
 
-def log(label: str, value: object | None = None) -> None:
-    """Send debugging print statements to stderr to avoid concatenating them with the puzzle answer sent to stdout."""
-    if value is None:
-        rich.print(label, file=sys.stderr)
-    else:
-        rich.print(label, value, file=sys.stderr)
-    # rich.print(f"{obj=}", file=sys.stderr)
+def log(*objects: object) -> None:
+    """Send debugging print statements to stderr with pretty formatting."""
+    rich.print(*objects, file=sys.stderr)
