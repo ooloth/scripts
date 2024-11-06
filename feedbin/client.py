284b68bbff74ec5616f0ebf2546ffdb1f57e19f9
Michael Uloth <hello@michaeluloth.com>
2024-11-05 21:20:50 -0500

    feedbin: outline client helpers + implement get_subscriptions

--- feedbin/client.py
+++ feedbin/client.py
@@ -1,15 +1,222 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Any
+
+import requests
+from pydantic import BaseModel
+
 from op.secrets import get_secret
+from utils.logs import log
+
+API = "https://api.feedbin.com/v2"
 
 _auth = None
 
 
-def get_auth() -> tuple[str, str]:
+def _get_auth() -> tuple[str, str]:
+    "Retrieve the username and password for the Feedbin API from 1Password."
     global _auth
 
     if _auth is None:
         username = get_secret("Feedbin", "username")
         password = get_secret("Feedbin", "password")
-
         _auth = (username, password)
 
     return _auth
+
+
+@dataclass
+class RequestArgs:
+    url: str
+    params: dict[str, Any] | None = None
+    json: dict[str, Any] | None = None
+    headers: dict[str, str] | None = None
+    # auth: tuple[str, str] | None = None
+
+
+class HTTPMethod(Enum):
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+
+
+class FeedbinError(Exception):
+    """Base class for Feedbin API errors."""
+
+    pass
+
+
+class NotFoundError(FeedbinError):
+    """Raised when a resource is not found."""
+
+    pass
+
+
+class UnexpectedError(FeedbinError):
+    "Raised for unexpected errors."
+
+    pass
+
+
+def make_request(method: HTTPMethod, args: RequestArgs) -> requests.Response:
+    """Make an HTTP request and handle common tasks."""
+    response = requests.request(
+        method.value,
+        args.url,
+        json=args.json,
+        params=args.params,
+        headers=args.headers,
+        auth=_get_auth(),
+    )
+    response.raise_for_status()
+    return response
+
+
+class Subscription(BaseModel):
+    title: str
+    site_url: str
+    feed_url: str
+    feed_id: int
+    id: int
+
+
+def get_subscriptions(*, extended: bool = False, since: datetime | None = None) -> list[Subscription]:
+    """
+    Get all subscriptions.
+
+    Params:
+    - extended: Include additional information about the subscription.
+    - since: Only return subscriptions that have been updated since this date.
+
+    DOCS: https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md#get-subscriptions
+    """
+    params = {"extended": extended, "since": ""}
+    if since:
+        params["since"] = since.isoformat()  # convert to ISO 8601 format
+
+    request_args = RequestArgs(
+        url=f"{API}/subscriptions.json",
+        params={"extended": extended, "since": since},
+    )
+
+    log.info(f"Feedbin: getting subscriptions with params: {params}")
+    response = make_request(HTTPMethod.GET, request_args)
+
+    match response.status_code:
+        case 200:
+            return [Subscription(**sub) for sub in response.json()]
+        case 404:
+            raise NotFoundError("Feedbin: no subscriptions found")
+        case _:
+            raise UnexpectedError("Feedbin: unexpected error while getting subscriptions")
+
+
+class FeedOption(BaseModel):
+    feed_url: str
+    title: str
+
+
+def create_subscription(url: str) -> Subscription | None:
+    """
+    Create a subscription.
+
+    DOCS: https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md
+
+    TODO: add params
+    TODO: add possible responses
+    """
+    request_args = RequestArgs(
+        url=f"{API}/subscriptions.json",
+        json={"feed_url": url},
+        headers={"Content-Type": "application/json; charset=utf-8"},
+    )
+    response = make_request(HTTPMethod.POST, request_args)
+    # match response.status_code:
+    #     case 200:
+    #         log(f"✅ Now subscribed to '{url}'")
+    return Subscription(**response.json())
+
+
+def delete_subscription(subscription_id: int) -> None:
+    """
+    Delete a subscription.
+
+    DOCS: ...
+
+    TODO: add params
+    TODO: add possible responses
+    """
+    request_args = RequestArgs(url=f"{API}/subscriptions/{subscription_id}.json")
+    make_request(HTTPMethod.DELETE, request_args)
+
+
+# TODO: move out of client file
+# def choose_between_multiple_feeds(feeds: list[FeedChoice]):
+#     """Prompt the user to choose between multiple feeds."""
+#     print("Multiple feeds found. Please choose one:")
+#     for idx, feed in enumerate(feeds, start=1):
+#         print(f"{idx}. {feed.title} ({feed.feed_url})")
+
+#     choice = int(input("Enter the number of the feed you want to subscribe to: "))
+#     if 1 <= choice <= len(feeds):
+#         selected_feed = feeds[choice - 1]
+#         print(f"Selected feed URL: {selected_feed.feed_url}")
+#     else:
+#         print("Invalid choice")
+
+
+#####################
+# Client as a class #
+#####################
+
+_client = None
+
+
+def get_client() -> "FeedbinClient":
+    global _client
+
+    if _client is None:
+        _client = FeedbinClient(*_get_auth())
+
+    return _client
+
+
+class FeedbinClient:
+    def __init__(self, username: str, password: str):
+        self.auth = (username, password)
+
+    def create_subscription(self, url: str) -> Subscription | None:
+        """Create a subscription and handle the response."""
+        response = requests.post(
+            f"{API}/subscriptions.json",
+            auth=self.auth,
+            headers={"Content-Type": "application/json; charset=utf-8"},
+            json={"feed_url": url},
+        )
+
+        if response.status_code == 201:
+            return Subscription(**response.json())
+        elif response.status_code == 302:
+            return Subscription(**response.json())
+        elif response.status_code == 404:
+            print(f"❌ No feed found at '{url}'")
+        elif response.status_code == 300:
+            self.choose_between_multiple_feeds(response.json())
+        else:
+            print(f"❌ Unexpected HTTP status code: {response.status_code}")
+            print(response.text)
+        return None
+
+    def choose_between_multiple_feeds(self, feeds: list[FeedOption]) -> None:
+        """Prompt the user to choose between multiple feeds."""
+        print("Multiple feeds found. Please choose one:")
+        for idx, feed in enumerate(feeds, start=1):
+            print(f"{idx}. {feed.title} ({feed.feed_url})")
+
+        choice = int(input("Enter the number of the feed you want to subscribe to: "))
+        if 1 <= choice <= len(feeds):
+            selected_feed = feeds[choice - 1]
+            print(f"Selected feed URL: {selected_feed.feed_url}")
+        else:
+            print("Invalid choice")
--- feedbin/subscribe.py
+++ feedbin/subscribe.py
@@ -1,59 +1,46 @@
-import json
+from dataclasses import asdict, dataclass
+from typing import Annotated, Literal
 
 import requests
 import typer
 from pydantic import BaseModel
 
-from feedbin.client import get_auth
+from feedbin.client import NotFoundError, Subscription, UnexpectedError, _get_auth, get_subscriptions
 from utils.logs import log
 
 # Docs:
-# - https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md
 # - https://typer.tiangolo.com/tutorial/subcommands/add-typer
 
 app = typer.Typer()
 
-API = "https://api.feedbin.com/v2"
-
-
-class Subscription(BaseModel):
-    title: str
-    site_url: str
-    feed_url: str
-    feed_id: int
-    id: int
-
-
-# @app.command(name="list")
-def ls() -> list[Subscription]:
-    """Fetch subscriptions and parse the relevant response properties using Pydantic."""
-    response = requests.get(f"{API}/subscriptions.json", auth=get_auth())
-    response.raise_for_status()
-
-    return [Subscription(**subscription) for subscription in response.json()]
-
 
 def _find_matching_subscription(url: str, subscriptions: list[Subscription]):
     url_without_trailing_slash = url.rstrip("/")
-    log("🔍 url_without_trailing_slash =", url_without_trailing_slash)
     for sub in subscriptions:
         if sub.site_url.strip("/") == url_without_trailing_slash:
             return sub
     return None
 
 
-def _validate_url(url: str | None) -> str | None:
-    if not url:
-        log("🚨 No URL provided.")
-        return None
+# def _validate_url(url: str) -> str:
+#     """Validate the URL provided by the user."""
+#     if len(url) == 0:
+#         url = typer.prompt("🚨 Please provide a URL")
+#         return _validate_url(url)
 
-    if not url.startswith("http"):
-        return f"https://{url}"
+#     if not url.startswith("http"):
+#         url = typer.prompt("🚨 Please provide full URL starting with 'https://' or 'http://")
+#         return _validate_url(url)
 
-    return url
+#     return url
 
 
-def choose_between_multiple_feeds(feeds: list[dict]):
+class FeedChoice(BaseModel):
+    feed_url: str
+    title: str
+
+
+def choose_between_multiple_feeds(feeds: list[FeedChoice]):
     """Prompt the user to choose between multiple feeds."""
     print("Multiple feeds found. Please choose one:")
     for idx, feed in enumerate(feeds, start=1):
@@ -62,53 +49,91 @@ def choose_between_multiple_feeds(feeds: list[dict]):
     choice = typer.prompt("Which feed number would you like to subscribe to?", type=int)
     if 1 <= choice <= len(feeds):
         selected_feed = feeds[choice - 1]
-        typer.echo(f"🔖 Selected feed URL: {selected_feed['feed_url']}")
+        log.info(f"🔖 Selected feed URL: {selected_feed['feed_url']}")
     else:
-        typer.echo("🚨 Invalid choice")
+        log.error("🚨 Invalid choice")
 
 
-@app.command()
-def add(url: str) -> Subscription | None:
+@dataclass
+class CreateSubscriptionRequestArgs:
+    url: Literal["https://api.feedbin.com/v2/subscriptions.json"]
+    json: dict[Literal["feed_url"], str]
+    auth: tuple[str, str]
+    headers: dict[Literal["Content-Type"], Literal["application/json; charset=utf-8"]]
+
+
+def create_subscription(url: str, dry_run: bool = False) -> Subscription | None:
     """
-    See: # See: https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md#create-subscription
+    Create a subscription and handle the response.
+    See: https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md#create-subscription
     """
-    # url = _validate_url(url)
-    # if not url:
-    # return
-
-    subscriptions = ls()
-    log("subscriptions:", subscriptions)
-
-    matching_subscription = _find_matching_subscription(url, subscriptions)
-    log("matching_subscription:", matching_subscription)
-
-    # if matching_subscription:
-    #     print("Matching subscription found:", matching_subscription)
-    # else:
-    #     print("No matching subscription found.")
-
-    """Create a subscription and handle the response."""
-    response = requests.post(
-        f"{API}/subscriptions.json",
-        auth=get_auth(),
+    request_args = CreateSubscriptionRequestArgs(
+        url="https://api.feedbin.com/v2/subscriptions.json",
+        json={"feed_url": url},
+        auth=_get_auth(),
         headers={"Content-Type": "application/json; charset=utf-8"},
-        data=json.dumps({"feed_url": url}),
     )
 
-    if response.status_code == 201:
-        print(f"✅ Successfully subscribed to '{url}'")
-        return Subscription(**response.json())
-    elif response.status_code == 302:
-        print(f"✅ Already subscribed to '{url}'")
-        return Subscription(**response.json())
-    elif response.status_code == 404:
-        print(f"❌ No feed found at '{url}'")
-    elif response.status_code == 300:
-        print(f"❌ Multiple feeds found at '{url}'")
-        choose_between_multiple_feeds(response.json())
-    else:
-        print(f"❌ Unexpected HTTP status code: {response.status_code}")
-        print(response.text)
+    dry_run = typer.confirm("🌵 Is this a dry run?", default=False)
+    if dry_run:
+        log.info("🌵 Dry run enabled. Skipping subscribing with this request:", request_args)
+        return None
+
+    response = requests.post(**asdict(request_args))
+    log.debug("🔍 response =", response.text)
+    log.debug("🔍 status =", response.status_code)
+
+    match response.status_code:
+        case 201:
+            log.info(f"✅ Now subscribed to '{url}'")
+            return Subscription(**response.json())
+        case 300:
+            log.error(f"❌ Multiple feeds found at '{url}'")
+            feeds = [FeedChoice(**choice) for choice in response.json()]
+            chosen_feed = choose_between_multiple_feeds(feeds)
+            return create_subscription(chosen_feed, dry_run)
+        case 302:
+            log.info(f"✅ Already subscribed to '{url}'")
+            return Subscription(**response.json())
+        case 404:
+            log.error(f"❌ No feed found at '{url}'")
+            return None
+        case _:
+            log.error("❌ Unexpected HTTP status code:", response.status_code)
+            log.error(response.text)
+            return None
+
+
+# See: https://github.com/feedbin/feedbin-api/blob/master/content/subscriptions.md#create-subscription
+@app.command()
+def main(url: str, dry_run: Annotated[bool, typer.Option("--dry-run", "-d")] = False) -> None:
+    try:
+        subscriptions = get_subscriptions()
+        log.info(f"{len(subscriptions)} Feedbin subscriptions found")
+    except NotFoundError as e:
+        log.error(e)
+        subscriptions = []
+    except UnexpectedError as e:
+        log.error(e)
+        subscriptions = []
+
+    # log.debug("🔍 subscriptions =", subscriptions)
+    return
+
+    url = typer.prompt("💬 What URL would you like to subscribe to?", type=str)
+    typer.confirm(f"🔖 Subscribe to '{url}'?", abort=True)
+
+    # TODO: can I skip this step and just try to add and then handle the response?
+    matching_subscription = _find_matching_subscription(url, subscriptions)
+
+    if matching_subscription:
+        log.info("👍 Subscription already exists:", matching_subscription)
+        raise typer.Abort()
+        # raise typer.Exit()
+
+    log.info("👎 No matching subscription found.")
+    create_subscription(url, dry_run)
+
     return None
 
 
