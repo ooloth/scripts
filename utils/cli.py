1ede42eeec5855c93efa1da811a770367064dfef
Michael Uloth <hello@michaeluloth.com>
2024-11-07 22:44:48 -0500

    utils(cli): make DryRun a bool

--- utils/cli.py
+++ utils/cli.py
@@ -1,19 +1,26 @@
 import os
 from pathlib import Path
+from typing import Annotated
 
+import typer
 from rich.table import Table
 
+DryRun = Annotated[bool, typer.Option("--dry-run", "-d", help="Dry run mode")]
 
-def get_env(dry_run: bool = False) -> dict[str, str]:
+
+# TODO: delete? not currently used... maybe useful for feed choices?
+def get_env(dry_run: str = "false") -> dict[str, str]:
     """Return updated environment variables to be used by the subprocess."""
     env = os.environ.copy()
+    dry_run_option: bool = dry_run.lower() == "true"
 
-    env["DRY_RUN"] = env.get("DRY_RUN", str(dry_run))
-    env["PYTHONPATH"] = Path(__file__).parent.absolute().as_posix()  # this project's root directory
+    env["DRY_RUN"] = env.get("DRY_RUN", str(dry_run_option).lower())
+    env["PYTHONPATH"] = Path.cwd().resolve().as_posix()  # this project's root directory
 
     return env
 
 
+# TODO: delete? not currently used... maybe useful for feed choices?
 def format_as_table(items: list[str], title: str) -> Table:
     """Display the available script options as a table."""
     table = Table(title=f"{title}:", show_header=False, title_justify="left", title_style="bold cyan")
