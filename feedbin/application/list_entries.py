52dca613afbd0fb0f85834483c7117de31cf98d0
Michael Uloth <hello@michaeluloth.com>
2024-11-14 22:22:37 -0500

    feedbin: call correct api helper in list_entries

--- feedbin/application/list_entries.py
+++ feedbin/application/list_entries.py
@@ -3,15 +3,22 @@ The core application logic for listing all entries of a Feedbin feed.
 """
 
 from common.logs import log
-from feedbin.adapters.api import NotFoundError, UnexpectedError
-from feedbin.adapters.api.subscriptions import Subscription, get_subscriptions
+from feedbin.adapters.api import ForbiddenError, NotFoundError, UnexpectedError
+from feedbin.adapters.api.entries import get_feed_entries
+from feedbin.domain.entities import Entry
 
 
-def list_feed_entries(feed_id: int) -> list[Subscription]:
+# NOTE: not currently called
+def list_feed_entries(feed_id: int) -> list[Entry]:
+    log.debug(f"🔍 Getting feed entries for feed {feed_id}")
+
     try:
-        subscriptions = get_subscriptions()
-        log.info(f"{len(subscriptions)} Feedbin subscriptions found")
-        return subscriptions
+        entries = get_feed_entries(feed_id)
+        log.info(f"{len(entries)} entries found")
+        return entries
+    except ForbiddenError as e:
+        log.error(e)
+        return []
     except NotFoundError as e:
         log.error(e)
         return []
