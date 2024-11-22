932136611eb539cb9bc602173734cc863fba1bd3
Michael Uloth <hello@michaeluloth.com>
2024-11-21 21:39:47 -0500

    rss: add placeholder for bulk adding subscriptions from a google sheet

---
+++ rss/subscriptions/add/sheets.py
@@ -0,0 +1,11 @@
+"""Handler for bulk subscribing to URLs saved to a Google Sheet."""
+
+# TODO: get authenticated sheets client
+# TODO: get URLs from sheet
+# TODO: subscribe to URLs
+# TODO: update status in sheet
+# TODO: for multiple choices, add options to sheet
+# TODO: for errors, add error message to sheet
+# TODO: for successful subscriptions, mark backlog unread
+# TODO: for successful subscriptions, append correct suffix to title
+# TODO: for successful subscriptions, add feed title to sheet
