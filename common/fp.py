c29fc67d73a1c7f38a0fa3e9db0c2ee1efe9dd59
Michael Uloth <hello@michaeluloth.com>
2024-11-29 20:05:34 -0500

    rss(sheets): use expression.pipe when processing rows

--- common/fp.py
+++ common/fp.py
@@ -6,32 +6,40 @@ See:
 """
 
 from functools import reduce
-from itertools import chain
 from typing import Callable, TypeVar
 
-A = TypeVar("A")
-B = TypeVar("B")
+from expression import pipe
+
+from common.logs import log
+
+_A = TypeVar("A")
+_B = TypeVar("B")
 
 
-def fmap(f: Callable[[A], B], xs: list[A]) -> list[B]:
+def fmap(f: Callable[[_A], _B], xs: list[_A]) -> list[_B]:
     return list(map(f, xs))
 
 
-def flatten(xs: list[list[A]]) -> list[A]:
+def flatten(xs: list[list[_A]]) -> list[_A]:
     return reduce(lambda acc, nested: acc + nested, xs)
-    return list(chain.from_iterable(xs))
+    # return list(chain.from_iterable(xs))
 
 
-def flatMap(f: Callable[[A], list[B]], xs: list[A]) -> list[B]:
+def flatmap(f: Callable[[_A], list[_B]], xs: list[_A]) -> list[_B]:
     return flatten(fmap(f, xs))
 
 
-def pipe(value: A, *functions: Callable[[A], A]) -> A:
-    """
-    Pass a value through a series of functions that take one argument of the same type.
+if __name__ == "__main__":
+    print(fmap(lambda x: x + 1, [1, 2, 3]))  # => [2, 3, 4]
+    print(flatten([[1, 2], [3, 4], [5, 6]]))  # => [1, 2, 3, 4, 5, 6]
+    print(flatmap(lambda x: [x, x + 1], [1, 2, 3]))  # => [1, 2, 2, 3, 3, 4]
+    result = pipe("1", int, float, str)  # => "1.0"
+    print(result)
 
-    .. code:: python
-       >>> # => executes: str(float(int('1')))
-       >>> assert flow("1", int, float, str) == "1.0"
-    """
-    return reduce(lambda acc, f: f(acc), functions, value)
+    pipeline = (int, float, str, float)
+    result2 = reduce(lambda acc, f: f(acc), (int, float, str, float), "1")
+    result3 = reduce(lambda acc, f: f(acc), pipeline, "1")
+    # result4 = flow("1", *pipeline)
+    log.debug(f"🔍 result2: {result2}")
+    log.debug(f"🔍 type result2: {type(result2)}")
+    # log.debug(f"🔍 result4: {result4}")
