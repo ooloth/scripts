8bedd5d7bc22e19fcd09c6e16cd45de113611f53
Michael Uloth <hello@michaeluloth.com>
2025-02-27 00:30:49 +0000

    tic-tac-toe: render board

--- tic_tac_toe/play.py
+++ tic_tac_toe/play.py
@@ -9,23 +9,52 @@ class Player:
 
 @dataclass(frozen=True)
 class Square:
-    pass
+    value: str = " "
 
 
 @dataclass(frozen=True)
 class Board:
-    pass
+    grid: tuple[tuple[Square, Square, Square]] = (
+        (Square(), Square(), Square()),
+        (Square(), Square(), Square()),
+        (Square(), Square(), Square()),
+    )
+
+    # def __eq__(self, other):
+    #     return self.squares == other.squares
+
+    def __str__(self):
+        rows = []
+        for row in self.grid:
+            rows.append("│ " + " │ ".join([square.value for square in row]) + " │")
+        return (
+            "┌───┬───┬───┐\n"
+            f"{rows[0]}\n"
+            "├───┼───┼───┤\n"
+            f"{rows[1]}\n"
+            "├───┼───┼───┤\n"
+            f"{rows[2]}\n"
+            "└───┴───┴───┘"
+        )
+
+    def render(self):
+        return str(self)
 
 
 @dataclass(frozen=True)
 class Game:
     board: Board | None = Board()
     players: set[Player] = field(default_factory=lambda: {Player("X"), Player("O")})
-    current_player: Player | None = Player("X")  # track turn in Player?
+    current_player: Player | None = Player("X")  # need this? track in Player?
     winner: Player | None = None
 
 
 @dataclass(frozen=True)
 class TicTacToe:
     def new_game(self) -> Game:
+        """Start a new game with an empty board."""
         return Game()
+
+    def load_game(self, board: Board) -> Game:
+        """Load an existing game with a given board."""
+        return Game(board=board)
--- tic_tac_toe/tests/test_play.py
+++ tic_tac_toe/tests/test_play.py
@@ -1,22 +1,62 @@
 """ """
 
-from tic_tac_toe.play import Board, Player, TicTacToe
+import textwrap
+
+from tic_tac_toe.play import Board, Player, Square, TicTacToe
 
 
 def test_when_game_starts_board_is_empty():
-    tic_tac_toe = TicTacToe()
-    new_game = tic_tac_toe.new_game()
-    new_board = Board()
-    assert new_game.board == new_board
+    new_game = TicTacToe().new_game()
+    assert all(square.value == " " for row in new_game.board.grid for square in row)
 
 
 def test_when_game_starts_there_are_two_players():
-    tic_tac_toe = TicTacToe()
-    new_game = tic_tac_toe.new_game()
+    new_game = TicTacToe().new_game()
     assert new_game.players == {Player("X"), Player("O")}
 
 
 def test_when_game_starts_player_x_goes_first():
-    tic_tac_toe = TicTacToe()
-    new_game = tic_tac_toe.new_game()
+    new_game = TicTacToe().new_game()
     assert new_game.current_player == Player("X")
+
+
+def test_board_renders_correctly_when_empty():
+    new_game = TicTacToe().new_game()
+    expected = textwrap.dedent(
+        """
+        ┌───┬───┬───┐
+        │   │   │   │
+        ├───┼───┼───┤
+        │   │   │   │
+        ├───┼───┼───┤
+        │   │   │   │
+        └───┴───┴───┘
+        """
+    ).strip()
+
+    assert new_game.board.render() == expected
+
+
+def test_board_renders_correctly_when_populated():
+    board = Board(
+        grid=(
+            (Square("X"), Square(" "), Square("O")),
+            (Square(" "), Square("O"), Square(" ")),
+            (Square(" "), Square(" "), Square("X")),
+        )
+    )
+    game = TicTacToe().load_game(board=board)
+
+    expected = textwrap.dedent(
+        """
+        ┌───┬───┬───┐
+        │ X │   │ O │
+        ├───┼───┼───┤
+        │   │ O │   │
+        ├───┼───┼───┤
+        │   │   │ X │
+        └───┴───┴───┘
+        """
+    ).strip()
+
+    assert game.board.render() == expected
